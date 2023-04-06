490de16f65900dc71b82b4ad15739b0350fd9d2c
samlv99 <samlv@miichisoft.net>
2023-04-06 10:22:44 +0700

    update

--- 3.js
+++ 3.js
@@ -2,43 +2,34 @@
 // Ví dụ: A = 1,2,3. Khi n = 2 thì kết quả có 3 cặp số, danh sách: 1&2, 1&3, 2&3. Khi n = 3 thì kết quả có 1 cặp số là 1,2,3.
 
 function ListCombinations(nums, n) {
-  let haha = [];
+  let output = [];
   nums = removeDuplicate(nums);
+  nums = nums.sort((a, b) => a - b);
+
   if (nums.length <= n) return nums;
 
-  // for (let i = 0; i < nums.length - 1; i++) {
-  //   for (let j = i + 1; j < nums.length; j++) {
-  //     haha.push([nums[i], nums[j]]);
-  //   }
-  // }
-
-  const backtrack = (index, str, n, j) => {
-    console.log(index);
-    console.log("n", n);
-    console.log("j", j);
+  const backtrack = (index, str, first) => {
     if (str.length === n) {
-      haha.push(str);
+      output.push(str);
       return;
     }
-    // if (index + 1 == n) return;
-    // if (nums[index] == nums[nums.length - 1]) return;
-    for (
-      let i = index;
-      i < (j !== undefined ? nums.length : nums.length - n + 1);
-      i++
-    ) {
-      if (str == nums[i]) {
+
+    for (let i = index; i < (first ? nums.length - n + 1 : nums.length); i++) {
+      if (
+        str == nums[i] ||
+        str.includes(nums[i]) ||
+        str[str.length - 1] >= nums[i]
+      ) {
         continue;
       }
-      index === 0
-        ? backtrack(index + 1, str + nums[i], n)
-        : backtrack(index + 1, str + nums[i], n, 1);
+
+      backtrack(index + 1, str + nums[i]);
     }
   };
 
-  backtrack(0, "", n);
+  backtrack(0, "", true);
 
-  return haha;
+  return output;
 }
 
 // xóa những số trùng nhau trong mảng đầu vào
@@ -56,5 +47,7 @@ function removeDuplicate(nums) {
   return result;
 }
 
-// console.log(ListCombinations([1, 1, 2, 7, 5, 4, 7, 19], 2));
-console.log(ListCombinations([1, 1, 2, 3], 2));
+console.log(ListCombinations([1, 1, 2, 7, 5, 4, 7, 9], 3));
+// console.log(ListCombinations([1, 1, 2, 3], 3));
+// console.log(ListCombinations([1, 2, 3, 4, 5], 3));
+// console.log(ListCombinations([1, 3, 2, 4, 5], 3));
